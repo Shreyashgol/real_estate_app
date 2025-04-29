208f171b0baa9f86077df6f8e904f847367f4587
Shreyash <shreyashgolhani1@Shreyashs-MacBook-Pro.local>
2025-04-29 23:53:01 +0530

    aaj ka kaaam

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import "./App.css";
 
 
 import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
       <Header/>
+      <About/>
+      <Projects/>
       
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,15 @@ import {assets} from '../assets/assets'
 const Header = () => {
   return (
     <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden'>
-      <img src={assets.header} alt="header" className='w-full h-full object-cover absolute -z-10' />
-      
+      <img src={assets.header_img} alt="header" className='w-full h-full object-cover absolute -z-10' />
+      <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'> 
+        <h2 className='text-2xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20 font-[Open-Sane]'>Explore Homes and Properties that fit for you</h2>
+        <div className='space-x-6 mt-16'>
+          <a href='#Projects' className='border border-white px-8 py-3 rounded'>Projects</a>
+          <a href='#Contact' className='bg-blue-600 border-white px-8 py-3 rounded'>Contact Us</a>
+        </div>
+
+      </div>
       <Navbar/>
 
 
