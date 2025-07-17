1512913cc94e91703a389f19cf967333c8937540
Shreyash Golhani <shreyashgolhani@Shreyashs-MacBook-Pro.local>
2025-07-17 22:54:00 +0530

    some chances done

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDmPvY6OSDy5VN5Xs9q3U00Jso00bCqaWw",
+  authDomain: "real-estate-app-2b881.firebaseapp.com",
+  projectId: "real-estate-app-2b881",
+  storageBucket: "real-estate-app-2b881.firebasestorage.app",
+  messagingSenderId: "408937378907",
+  appId: "1:408937378907:web:5dc708d0bef6235a4f3b7e",
+  measurementId: "G-71365E0FTB"
 };
 
 const app = initializeApp(firebaseConfig);
